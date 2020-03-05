e1a21a59bddbb6f6a437af6df56ff942bf127ac9
Wenwen Zhang <Wenwen.Zhang@FCStone.com>
2020-03-04 20:20:14 -0600

    0304

--- epi_judge_cpp/even_odd_list_merge.cc
+++ epi_judge_cpp/even_odd_list_merge.cc
@@ -1,8 +1,26 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
+
+
+using std::array;
+
 shared_ptr<ListNode<int>> EvenOddMerge(const shared_ptr<ListNode<int>>& L) {
-  // TODO - you fill in here.
-  return nullptr;
+  if (L == nullptr) return L;
+  
+  auto even_dummy_head = make_shared<ListNode<int>>(0, nullptr);
+  auto odd_dummy_head = make_shared<ListNode<int>>(0, nullptr);
+
+  array<shared_ptr<ListNode<int>>, 2> tails = {even_dummy_head, odd_dummy_head};
+  
+  int turn = 0;
+  for (auto iter = L; iter; iter = iter->next) {
+    tails[turn]->next = iter;
+    tails[turn] = tails[turn]->next;
+    turn ^= 1;  // Alternate between even and odd.
+  }
+  tails[1]->next = nullptr;
+  tails[0]->next = odd_dummy_head->next;
+  return even_dummy_head->next;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/is_list_palindromic.cc
+++ epi_judge_cpp/is_list_palindromic.cc
@@ -1,10 +1,68 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
+#include <memory>
 
-bool IsLinkedListAPalindrome(shared_ptr<ListNode<int>> L) {
-  // TODO - you fill in here.
+using std::shared_ptr;
+
+// shared_ptr<ListNode<int>> ReverseList(shared_ptr<ListNode<int>> &);
+
+bool IsLinkedListAPalindrome(shared_ptr<ListNode<int>>& L) {
+  if (L == nullptr) return true;
+
+  //   shared_ptr<ListNode<int>> slow = L, fast = L;
+  // while (fast && fast->next) {
+  //   fast = fast->next->next, slow = slow->next;
+  // }
+
+  // // Compares the first half and the reversed second half lists.
+  // auto first_half_iter = L;
+
+  // shared_ptr<ListNode<int>> prev = nullptr, curr = slow;
+
+  // while (curr) {
+  //   auto temp = curr->next;
+  //   curr->next = prev;
+  //   prev = curr;
+  //   curr = temp;
+  // }
+  // auto second_half_iter = prev;
+  
+  // //  second_half_iter = ReverseList(slow);
+  // while (second_half_iter && first_half_iter) {
+  //   if (second_half_iter->data != first_half_iter->data) {
+  //     return false;
+  //   }
+  //   second_half_iter = second_half_iter->next;
+  //   first_half_iter = first_half_iter->next;
+  // }
+  // return true;
+
+  shared_ptr<ListNode<int>> slow = L, fast = L;
+  while (fast && fast->next) {
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+  
+  
+  auto first_half_iter = L;
+  shared_ptr<ListNode<int>> prev = nullptr, curr = slow;
+
+  while (curr) {
+    auto temp = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = temp;
+  }
+  auto second_half_iter = prev;
+
+  while (first_half_iter && second_half_iter) {
+    if (first_half_iter->data != second_half_iter->data) return false;
+    first_half_iter = first_half_iter->next;
+    second_half_iter = second_half_iter->next;
+  }
   return true;
 }
+
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
